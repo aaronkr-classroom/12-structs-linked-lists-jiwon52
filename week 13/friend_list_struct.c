d0bef77241f2ffdff705cc7cc052c4d25f354f05
ththfql <bona5141@naver.com>
2025-12-02 16:53:59 +0900

    finish!!!!!

---
+++ week 13/friend_list_struct.c
@@ -0,0 +1,106 @@
+﻿//friend_list_struct.c
+#include<stdio.h>
+
+#define MAX_FRIENDS 5
+
+typedef char NAME_TYPE[21]; //20문자 + NULL문자
+typedef unsigned short int us;
+typedef struct {
+	NAME_TYPE name;
+	us age;
+	float height;
+	float weight;
+} Person;
+
+int ADDfriend(Person* p_friend, int count) {
+	// 입력 가능한 최대수를 넘었는지 확인
+	if (count < MAX_FRIENDS) {
+		//friends 배열의 count 위치로 이동함
+		p_friend = p_friend + count;
+
+
+		printf("\n 새로운 친구의 정보를 입력하세요.\n");
+		printf("1.이름: >>>");
+		scanf_s("%s", &p_friend->name, sizeof(p_friend->name));
+
+		printf("2.나이: >>>");
+		scanf_s("%hu", &p_friend->age);
+
+		printf("키: >>>");
+		scanf_s("%f", &p_friend->height);
+
+		printf("몸무게: >>>");
+		scanf_s("%f", &p_friend->weight);
+
+		printf("\n입력한 정보는 다음과 같습니다.\n");
+		return 1; //True
+	}
+	else {
+		printf("최대 인원을 초과하여 입력을 할 수 없다!\n");
+		printf("최대 %d명까지만 관리 가능합니다.", MAX_FRIENDS);
+		return 0; //False
+	}
+}
+
+void ShowFriendList(Person *p_friend, int count){
+	NAME_TYPE* p_name, us* p_age,
+	float* p_height, float* p_weight, int count
+) {
+	//친구가 있는지 확인
+	if (count > 0) {
+		printf("\n등록된 친구 목록\n");
+		printf("==============================\n");
+		for (int i = 0; i < count; i++) {
+			printf("%21s, %3hu, %6.2f, %6.2f\n",
+				p_friend->name,
+				p_friend->age,
+				p_friend->height,
+				p_friend->weight);
+			p_friend++; //다음 위치로 이동함
+		}
+		printf("==============================\n");
+	}
+	else {
+		printf("\n등록된 친구가 없다!\n\n");
+	}
+}
+
+int main(void) {
+	//배열 선언
+	Person friend[MAX_FRIENDS];
+	int count = 0,
+		menu_num;
+
+	while (1) {
+		//선택하고 싶은 메뉴
+		printf("\t[ 메인 메뉴 ] \n");
+		printf("==============================\n");
+		printf("1.친구 추가\n");
+		printf("2.친구 목록 보기\n");
+		printf("3.종료\n");
+		printf("==============================\n");
+		printf("번호 선택");
+		scanf_s("%d", &menu_num);
+
+		//while문에서 나가야겟다
+		if (menu_num == 3) break;
+
+		switch (menu_num) {
+		case 1:
+			if (ADDfriend(friends, count)) {
+				count++;
+			}
+			break;
+		case 2:
+			ShowFriendList(friends, count);
+			break;
+
+		default:
+			printf("1-3번만 입력 가능합니다");
+
+		}
+
+
+	}
+	return 0;
+}
---
+++ week 13/linked.c
@@ -0,0 +1,63 @@
+﻿//linked.c (slide 62)
+#include<stdio.h>
+#include<malloc.h>
+
+typedef struct {
+	int number;
+	struct Node* p_next; // 다음 노드를 가리킬 포인터
+} Node;
+
+void AddNumber(Node **pp_head, Node **pp_tail, int data) {
+	if (NULL != *pp_head) { //이미 노드가 있으면
+		(*pp_tail)->p_next = (Node*)malloc(sizeof(Node));
+		*pp_tail = (*pp_tail)->p_next; // 새 노드의 주소
+	}
+	else { //노드 데이터가 없다..!
+		// *p_head 값이 NULL이라서 첫 노드가 추가함
+		*pp_head = (Node*)malloc(sizeof(Node));
+		*pp_tail = *pp_head;
+	}
+	(*pp_tail)->number = data; // 숫자 저장함
+	(*pp_tail)->p_next = NULL; // 마지막 노드이어서
+}
+
+int main(void) {
+	Node* p_head = NULL, * p_tail = NULL, * p;
+	int sum = 0, temp;
+
+	// ========= #1.숫자 입력 ===========
+	//무한루프:중간에 9999를 누르면 종료
+	while (1) {
+		printf("숫자를 입력하세요 (9999를 누르면 종료):");
+		scanf_s("%d", &temp);
+
+		if (9999 == temp) break;
+
+		//노드의 시작과 끝을 기억하는 포인터의 주소 값과 입력된 숫자를 전달
+		AddNumber(&p_head, &p_tail, temp);
+	}
+
+	// ========= #2. 모든 입력 받은 숫자의 합 =========
+	// 입력된 숫자를 출력하기 위해서 노드를 탐색할 포인터에 시작 노드의 주소 값을 대입합니다.
+	p = p_head;
+	while(NULL != p); {
+		if (p != p_head) {
+			printf(" + "); //숫자와 숫자 사이에 + 출력
+		}
+		printf("%d", p->number);
+
+		sum = sum + p->number;
+		p = p->p_next; //다음 노드로 이동
+	}
+	printf(" =%d\n", sum);
+
+	// ========= #3. 메모리 해제 ============
+	while (NULL != p_head) {
+		p = p_head; //현재 노드를 삭제하기 위해 p변수에 노드 주소 값을 저장
+		p_head = p_head->p_next; //시작 위치를 다음 노드로 이동
+		free(p); //기억했던 주소를 사용하여 노드 삭제함
+	}
+	p_tail = p_tail; //반복문을 나오면 p_head값은 NULL. p_tail도 NULL로 변경.
+
+	return 0;
+}
